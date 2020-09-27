331e7d47de335f37ecfae28b6c715ffe2124c711
Victor Petsev <Vpetsev96@gmail.com>
2020-09-27 15:00:19 -0500

    URLConverters, Request Methods, and Data Headers

--- app.py
+++ app.py
@@ -1,11 +1,87 @@
-from flask import Flask
+from flask import Flask, url_for, request, json
 
 app = Flask(__name__)
 
-
+# Simple flask.route decorator specifying route
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+# Another simple flask.route decorator without additional options
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+
+# The flask.route decorator can be replaced with
+# specified converters with <converter:name>
+
+# By default the route is a string - which accepts text without slashes
+# @app.route('/articles/<article_id>')
+@app.route('/articles/<int:article_id>')
+@app.route('/articles/<float:article_id>')
+@app.route('/articles/<path:article_id>')
+def api_article(article_id):
+    return 'You are reading ' + article_id
+
+
+# GET Parameters
+    # Import request from flash
+@app.route('/hello')
+def api_hello():
+    # GET /hello?name=Victor
+    # > Hello Victor
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello John Doe'
+
+
+# Request Methods (HTTP Verbs)
+@app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == "GET":
+        return "ECHO: POST\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PATCH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+
+# Request Data and Headers
+    # import 'json' from flask
+    # Files from forms can be posted via an HTML form with "request.files"
+@app.route('/messages', methods=['POST'])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        """
+            Request.data returns bytes? 
+        """
+        return "Text Message: " + str(request.data)
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'applications/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    elif request.files:
+        return "File was received."
+
+    else:
+        return "415 Unsupported Media Type ;)"
 
 
 if __name__ == '__main__':
---
+++ hello.py
@@ -0,0 +1,3 @@
+from flask import request
+
+@app.route('/hello')
